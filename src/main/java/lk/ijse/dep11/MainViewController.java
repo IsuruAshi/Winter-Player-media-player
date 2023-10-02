82f14916eff6692e93674600d7af9308fc251a18
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-10-02 09:43:25 +0530

    Set MainScene.fxml and MainViewController.java

---
+++ src/main/java/lk/ijse/dep11/MainViewController.java
@@ -0,0 +1,37 @@
+package lk.ijse.dep11;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.MediaView;
+
+public class MainViewController {
+    public AnchorPane root;
+    public TextField txtBrowse;
+    public Button btnBrowse;
+    public MediaView mvMain;
+    public Button btnPlay;
+    public Button btnStop;
+    public Button btnFast;
+    public Button btnNormal;
+    public Button btnPause;
+
+    public void btnBrowseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlayOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStopOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFastOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNormalOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPauseOnAction(ActionEvent actionEvent) {
+    }
+}
