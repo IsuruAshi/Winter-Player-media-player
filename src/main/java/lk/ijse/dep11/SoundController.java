c268ef88901c34773f96dd75642dcf53fef1424a
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-10-02 10:56:48 +0530

    Add sound button and SoundScene

---
+++ src/main/java/lk/ijse/dep11/SoundController.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep11;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.media.MediaPlayer;
+
+public class SoundController {
+    public Slider sliderSound;
+    public Label lblSoundLevel;
+
+    MediaPlayer mediaPlayer;
+
+
+}
